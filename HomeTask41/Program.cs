a43a71f5fbe269f633a38e91b8507021a83cbec8
Ilya Besedin <besedka07@icloud.com>
2022-12-20 19:51:10 +0300

    HomeWork6

---
+++ HomeTask41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+int CountPositiveNumbers(int[] numbers)
+
+{
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] > 0) count++;
+    }
+    return count;
+}
+int[] CreateArray(int countElement)
+{
+    int[] array = new int[countElement];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент под индексом {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (i < numbers.Length - 1) Console.Write($"{numbers[i]},");
+        else Console.Write($"{numbers[i]}");
+    }
+}
+Console.WriteLine("Введите размер массива");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] mas = CreateArray(number);
+int res = CountPositiveNumbers(mas);
+PrintArray(mas);
+Console.WriteLine($" -> {res}");
---
+++ HomeTask43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку
+// пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+// k1*x+b1=k2*x+b2
+// k1*x-(k2*x)=b2-b1
+// x(k1-k2)=b2-b1
+// x=(b2-b1)/(k1-k2)
+double CalcX(double bx1, double kx1, double bx2, double kx2)
+{
+return (bx2 - bx1) / (kx1 - kx2);
+}
+double CalcY(double x, double bx1, double kx1, double bx2, double kx2)
+{
+return kx1 * x + bx1;
+}
+Console.WriteLine("ЗАДАНЫ ДВЕ ПРЯМЫЕ 'y = k1 * x + b1' и 'y = k2 * x + b2'");
+Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+if (k1 != k2)
+{
+double x = Math.Round(CalcX(b1, k1, b2, k2), 1, MidpointRounding.ToZero);
+double y = Math.Round(CalcY(x, b1, k1, b2, k2), 1, MidpointRounding.ToZero);
+Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x};{y})");
+}
+else Console.WriteLine("Прямые параллельны");
