39b4930d31bd80c66dda5fad0dd007deb22d817b
Ilya Besedin <besedka07@icloud.com>
2022-12-29 23:38:26 +0300

    HomeWork9

---
+++ HomeTask66/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumInterval1(int num1, int num2)
+{
+    if (num1 >= num2) return num1 + SumInterval1(num1 - 1, num2);
+    else return 0;
+
+}
+int SumInterval2(int num1, int num2)
+{
+    if (num1 <= num2) return num1 + SumInterval2(num1 + 1, num2);
+    else return 0;
+
+}
+Console.WriteLine("Введите натуральное число (M): ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число (N): ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+if (number1 > number2) result = SumInterval1(number1, number2);
+else result = SumInterval2(number1, number2);
+Console.WriteLine(result);
---
+++ HomeTask68/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 68: Напишите программу вычисления функции 
+// Аккермана с помощью рекурсии. Даны два неотрицательных 
+// числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+// функция ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+int Akkerman(int nN, int mM)
+{
+    if (nN == 0) return mM + 1;
+    else if (mM == 0) return Akkerman(nN - 1, 1);
+    return Akkerman(nN - 1, Akkerman(nN, mM - 1));
+}
+Console.WriteLine("Введите натуральное число (m): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число (n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m < 4 && n < 12 && m >= 0 && n >= 0)
+{
+    int result = Akkerman(m, n);
+    Console.Write($"m = {m}, n = {n} -> A({m},{n}) = {result}");
+}
